6f6af077d60a5e7a485f467c691aa380aff81c67
021800527 <978621025@qq.com>
2021-05-02 00:46:21 +0800

    稍微测试了一下vue - pdf

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div id="app">
-    <el-upload>123</el-upload>
+  <div>
+    <div style="background: transparent;height: 100px;">
+      12312312
+      <pdf
+        ref="pdf"
+        :src="url"
+        style="z-index: -1"
+      >
+      </pdf>
+    </div>
   </div>
 </template>
 
 <script>
+import pdf from 'vue-pdf'
 export default {
-  name: 'App'
+  components: {
+    pdf
+  },
+  data: function () {
+    return {
+      url: 'https://dakaname.oss-cn-hangzhou.aliyuncs.com/file/2018-12-28/1546003237411.pdf'
+    }
+  }
 }
 </script>
 
